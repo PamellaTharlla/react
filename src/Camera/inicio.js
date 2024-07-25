5d1bcc99c9f9add0b02bc53dc496347a99776caf
PamellaTharlla <pamellatharlla90@gmail.com>
2024-07-25 15:42:54 -0300

    atualização

---
+++ src/Camera/inicio.js
@@ -0,0 +1,143 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { Camera, CameraType } from 'expo-camera/legacy';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function App() {
+  const [tipo, setTipo] = useState(CameraType.back);
+  const [permissao, solicitarPermissao] = Camera.useCameraPermissions();
+  const [foto, setFoto] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    if (permissao && !permissao.granted) {
+      solicitarPermissao();
+    }
+  }, [permissao]);
+
+  if (!permissao) {
+    // As permissões da câmera ainda estão carregando
+    return <View />;
+  }
+
+  if (!permissao.granted) {
+    // As permissões da câmera não foram concedidas
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Precisamos da sua permissão para mostrar a câmera</Text>
+        <TouchableOpacity onPress={solicitarPermissao} style={styles.permissionButton}>
+          <Text style={styles.permissionButtonText}>Conceder permissão</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  async function alternarTipoDeCâmera() {
+    setTipo(tipoAtual => 
+      tipoAtual === CameraType.back 
+        ? CameraType.front 
+        : CameraType.back
+    );
+  }
+
+  async function tirarFoto() {
+    if (cameraRef.current) {
+      const { uri } = await cameraRef.current.takePictureAsync();
+      setFoto(uri);
+    }
+  }
+
+  function voltarParaCamera() {
+    setFoto(null);
+  }
+
+  return (
+    <View style={styles.container}>
+      {foto ? (
+        <View style={styles.previewContainer}>
+          <Image source={{ uri: foto }} style={styles.foto} />
+          <TouchableOpacity style={styles.voltarButton} onPress={voltarParaCamera}>
+            <Ionicons name="arrow-back" size={40} color="white" />
+            <Text style={styles.buttonText}>Voltar para a Câmera</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <Camera style={styles.camera} type={tipo} ref={cameraRef}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={alternarTipoDeCâmera}>
+              <Ionicons 
+                name={tipo === CameraType.back ? 'camera-reverse' : 'camera-front-outline'} 
+                size={40} 
+                color="white" 
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={tirarFoto}>
+              <Ionicons name="camera" size={60} color="white" />
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'black', // Adiciona um fundo preto para melhorar a visibilidade
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 20,
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 5,
+  },
+  foto: {
+    width: '100%',
+    height: 300,
+    marginTop: 10,
+  },
+  previewContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  voltarButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ff4081', // Cor de fundo do botão voltar
+    padding: 10,
+    borderRadius: 8,
+    margin: 20,
+  },
+  permissionButton: {
+    backgroundColor: '#ff4081', // Cor de fundo do botão de permissão
+    padding: 10,
+    borderRadius: 8,
+  },
+  permissionButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
---
+++ src/Home/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+      <Text style={styles.text}>by camera</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Camera')}
+      >
+        <Text style={styles.buttonText}>Abrir Camera</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#DDA0DD',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  },
+  button: {
+    backgroundColor: '#8A2BE2', // Cor do botão
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
